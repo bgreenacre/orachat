7b0ce19065c8a48f19e3a58b90407b64b1a1cb24
Brian Greenacre <bgreenacre42@gmail.com>
2016-12-12 21:50:11 -0600

    Initial commit of the Ora Chat API project.

---
+++ site/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ora\Chat\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Ora\Chat\Events\SomeEvent' => [
+            'Ora\Chat\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
