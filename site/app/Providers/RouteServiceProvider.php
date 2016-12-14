a081d05ef0a67e67d1fe63382b9e39b04031813e
Brian Greenacre <bgreenacre42@gmail.com>
2016-12-13 22:56:07 -0600

    Editing the routes provider in preparation of making API routes.

--- site/app/Providers/RouteServiceProvider.php
+++ site/app/Providers/RouteServiceProvider.php
@@ -36,27 +36,6 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-
-        //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::group([
-            'middleware' => 'web',
-            'namespace' => $this->namespace,
-        ], function ($router) {
-            require base_path('routes/web.php');
-        });
     }
 
     /**
@@ -68,12 +47,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::group([
+        $router = $this->app->make(Router::class);
+        $router->group([
             'middleware' => 'api',
             'namespace' => $this->namespace,
             'prefix' => 'api',
         ], function ($router) {
-            require base_path('routes/api.php');
         });
     }
 }
